89ae3770cb2f30e6edcb998b5587a08f245be067
Naveed Ahmad Hematmal <NaveedAhmadHematmal@gmail.com>
2020-05-03 16:40:56 +0430

    Maven repo initialized

---
+++ src/test/java/com/ihs_lms/webapp/WebAppTests.java
@@ -0,0 +1,13 @@
+package com.ihs_lms.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
