d95c4321beefc43b4715947fd64083e8615053ca
Naveed Ahmad Hematmal <NaveedAhmadHematmal@gmail.com>
2020-06-02 16:17:04 +0430

    feat(routes): add routes for all jsp pages

--- src/main/java/com/ihs_lms/controllers/MainController.java
+++ src/main/java/com/ihs_lms/controllers/MainController.java
@@ -11,28 +11,92 @@ public class MainController {
     public String index() {    
       return "index";
     }
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
     public String dashboard() {    
       return "dashboard";
     }
     @RequestMapping(value = "/faculty", method = RequestMethod.GET)
     public String faculty() {    
-      return "dashboard";
+      return "faculty";
     }
-    @RequestMapping(value = "/employee", method = RequestMethod.GET)
-    public String employee() {    
-      return "dashboard";
+    @RequestMapping(value = "/faculty/student-nightshift", method = RequestMethod.GET)
+    public String sns() {    
+      return "student-nightshift";
+    }
+    @RequestMapping(value = "/faculty/teacher-coordinator", method = RequestMethod.GET)
+    public String tc() {    
+      return "teacher-coordinator";
+    }
+    @RequestMapping(value = "/faculty/timetable", method = RequestMethod.GET)
+    public String timetable() {    
+      return "timetable";
+    }
+    @RequestMapping(value = "/faculty/exam-timetable", method = RequestMethod.GET)
+    public String exam_timetable() {    
+      return "exam-timetable";
+    }
+    @RequestMapping(value = "/faculty/course", method = RequestMethod.GET)
+    public String course() {    
+      return "course";
+    }
+    @RequestMapping(value = "/staff", method = RequestMethod.GET)
+    public String staff() {    
+      return "employee";
+    }
+    @RequestMapping(value = "/staff/teacher", method = RequestMethod.GET)
+    public String teacher() {    
+      return "teacher";
+    }
+    @RequestMapping(value = "/staff/labor", method = RequestMethod.GET)
+    public String labor() {    
+      return "labor";
+    }
+    @RequestMapping(value = "/staff/educational-background", method = RequestMethod.GET)
+    public String seb() {    
+      return "staff-educational-background";
+    }
+    @RequestMapping(value = "/staff/experience", method = RequestMethod.GET)
+    public String experience() {    
+      return "experience";
     }
     @RequestMapping(value = "/report", method = RequestMethod.GET)
     public String report() {    
-      return "dashboard";
+      return "student-list";
     }
     @RequestMapping(value = "/student", method = RequestMethod.GET)
     public String student() {    
-      return "dashboard";
+      return "student";
+    }
+    @RequestMapping(value = "/student/job", method = RequestMethod.GET)
+    public String job() {    
+      return "student-job";
+    }
+    @RequestMapping(value = "/student/score", method = RequestMethod.GET)
+    public String score() {    
+      return "score";
+    }
+    @RequestMapping(value = "/student/attendance", method = RequestMethod.GET)
+    public String attendance() {    
+      return "student-attendance";
     }
     @RequestMapping(value = "/hostel", method = RequestMethod.GET)
     public String hostel() {    
-      return "dashboard";
+      return "hostel";
+    }
+    @RequestMapping(value = "/hostel/room", method = RequestMethod.GET)
+    public String room() {    
+      return "hostel-room";
+    }
+    @RequestMapping(value = "/hostel/registration", method = RequestMethod.GET)
+    public String registration() {    
+      return "hostel-registration";
+    }
+    @RequestMapping(value = "/hostel/attendance", method = RequestMethod.GET)
+    public String hattendance() {    
+      return "hostel-attendance";
+    }
+    @RequestMapping(value = "/hostel/employee", method = RequestMethod.GET)
+    public String hemployee() {    
+      return "hostel-employee";
     }
 }
