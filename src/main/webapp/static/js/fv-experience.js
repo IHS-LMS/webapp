524809e85669bdc5dea6975e3e89f0b739830990
Elham Karbasi <57432784+Elham-karbasi@users.noreply.github.com>
2020-07-29 23:45:06 +1200

    The js code of Experience has been added.

---
+++ src/main/webapp/static/js/fv-experience.js
@@ -0,0 +1,30 @@
+
+import { allLetter, allNumber, ValidateEmail } from "validation-functions.js";
+
+function formValidation() {
+    var orgainizationName= document.Experience.Orgainization_Name;
+    var organizationLocation = document.Experience.Organization_Location;
+    var startDate = document.Experience.Start_Date;
+    var endDate = document.Experience.End_Date;
+    var organizationCell = document.Experience.Organization_Cell;
+    var organizationEmail = document.Experience.Organization_Email;
+    if (allLetter(orgainizationName , 'Orgainization Name must have alphabet characters only')) {
+        if (allLetter(organizationLocation, 'Invalid Organization Location!')) {
+            if (startDate.value == "" ? alert('Invalid Start Date!') : true) {
+                if (endDate.value == "" ? alert('Invalid End Date!') : true) {
+                    if (allNumber(organizationCell, 'Invalid organization Phone Number!')) {
+                        if (ValidateEmail(organizationEmail, 'You have entered an invalid email address!')) {
+                            alert("Form Sucessfully Submitted!");
+                                window.location.reload();
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                        
+    return false;
+  }
+document.Experience.onsubmit = function () {
+    formValidation();
+}
