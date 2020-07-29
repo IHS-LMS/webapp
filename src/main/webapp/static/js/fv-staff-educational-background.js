3ecabd767bd79c46f097282d82c6e3d751adc328
Elham Karbasi <57432784+Elham-karbasi@users.noreply.github.com>
2020-07-29 23:46:46 +1200

    The js code of Staff-Educational-Background has been added.

---
+++ src/main/webapp/static/js/fv-staff-educational-background.js
@@ -0,0 +1,30 @@
+
+import { allLetter, checkSelectList, checkFileExtension } from "validation-functions.js";
+
+function formValidation() {
+    var staffName= document.Staff_Educational_Background.Staff_Name;
+    var degree = document.Staff_Educational_Background.Degree;
+    var campusName = document.Staff_Educational_Background.Campus_Name;
+    var campusLocation = document.Staff_Educational_Background.Campus_Location;
+    var graduationDate = document.Staff_Educational_Background.Graduation_Date;
+    var transcript = document.Staff_Educational_Background.Transcript;
+    if (checkSelectList(staffName, 'Select Valid Staff Name!')) {
+        if (allLetter(degree, 'Invalid Degree')) {
+            if (allLetter(campusName , 'campus Name must have alphabet characters only')) {
+                if (allLetter(campusLocation, 'Invalid Campus Location!')) {
+                    if (graduationDate.value == "" ? alert('Invalid graduationDate!') : true) {
+                        if (checkFileExtension(transcript, 'Invalid photo Format!')) {
+                            alert("Form Sucessfully Submitted!");
+                                window.location.reload();
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                        
+    return false;
+  }
+document.Staff_Educational_Background.onsubmit = function () {
+    formValidation();
+}
